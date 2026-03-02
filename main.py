f03a164ec025fac0441c49812a40b2e228acd437
Jason <castrob.jason@gmail.com>
2026-03-02 17:06:47 -0600

    fix CORS produccion

--- main.py
+++ main.py
@@ -9,7 +9,11 @@ app = FastAPI(title="Fitness Tracker API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://localhost:3001"],
+    allow_origins=[
+        "http://localhost:3000",
+        "http://localhost:3001",
+        "https://fitness-tracker-loye.vercel.app"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
