fdaa3ca0cbfe9ffbecbfc50791f5aac0865f1a49
Jason <castrob.jason@gmail.com>
2026-03-02 21:05:45 -0600

    remover endpoints admin

--- main.py
+++ main.py
@@ -26,13 +26,3 @@ app.include_router(workouts.router, prefix="/api")
 @app.get("/")
 def root():
     return {"message": "Fitness Tracker API funcionando 🚀"}
-
-@app.get("/admin/users")
-def get_all_users(db: Session = Depends(get_db)):
-    users = db.query(User).all()
-    return [{"id": u.id, "name": u.name, "email": u.email} for u in users]
-
-@app.get("/admin/workouts")
-def get_all_workouts(db: Session = Depends(get_db)):
-    workouts = db.query(Workout).all()
-    return [{"id": w.id, "user_id": w.user_id, "type": w.type, "duration": w.duration, "date": str(w.date)} for w in workouts]
